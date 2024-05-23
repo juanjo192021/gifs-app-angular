ba0220b18d65744f48c29515bfdedd2fb8a4dbbf
Juan José Pérez Lazarte <juanjo101619@gmail.com>
2024-05-22 19:18:56 -0500

    Actualización del gifs.service.ts

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
-import { environment } from '../../../environments/environment.prod';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
 
